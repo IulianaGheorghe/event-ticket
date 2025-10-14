b074c2f59a2470e612926f18db1cebff8033f0c7
Iulia Gheorghe <iuliagheorghe2001@gmail.com>
2025-10-14 23:32:05 +0300

    Add service logic to list events for organizer

--- backend/src/main/java/io/github/IulianaGheorghe/event_ticket/services/EventService.java
+++ backend/src/main/java/io/github/IulianaGheorghe/event_ticket/services/EventService.java
@@ -2,9 +2,12 @@ package io.github.IulianaGheorghe.event_ticket.services;
 
 import io.github.IulianaGheorghe.event_ticket.domain.CreateEventRequest;
 import io.github.IulianaGheorghe.event_ticket.domain.entities.Event;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.UUID;
 
 public interface EventService {
     Event createEvent(UUID organizerID, CreateEventRequest event);
+    Page<Event> listEventsForOrganizer(UUID organizerId, Pageable pageable);
 }
