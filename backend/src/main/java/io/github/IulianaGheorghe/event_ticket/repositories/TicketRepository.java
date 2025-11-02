9e3d7e6150dbe82cd2a298aaf8cb89571a9704e1
Iulia Gheorghe <iuliagheorghe2001@gmail.com>
2025-11-02 23:23:21 +0200

    Create ticket and ticket type repositories

---
+++ backend/src/main/java/io/github/IulianaGheorghe/event_ticket/repositories/TicketRepository.java
@@ -0,0 +1,12 @@
+package io.github.IulianaGheorghe.event_ticket.repositories;
+
+import io.github.IulianaGheorghe.event_ticket.domain.entities.Ticket;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface TicketRepository extends JpaRepository<Ticket, UUID> {
+    int countByTicketTypeId(UUID ticketTypeId);
+}
